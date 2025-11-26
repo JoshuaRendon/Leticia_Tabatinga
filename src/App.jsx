3a4dd0848c4c9b683df401166564a619c4653d4a
JoshuaRendon <jorendona@unal.edu.co>
2025-11-26 11:47:29 -0500

    Versión inicial del proyecto React

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "20px" }}>
+      <h1>Geo Viewer</h1>
+      <p>Página base para mapas y PDFs.</p>
+    </div>
+  );
+}
